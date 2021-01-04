927906487135232a697ab0f5058bb013907dab33
I'M SBH <615518126@qq.com>
2021-01-04 12:53:49 +0800

    SBH NEW VERSION2

---
+++ app/src/main/java/com/NcuWeather/android/logic/model/Weather.kt
@@ -0,0 +1,3 @@
+package com.NcuWeather.android.logic.model
+
+data class Weather(val realime: RealtimeResponse.Realtime, val dAILY:DailyResponse.Daily)
---
+++ app/src/main/java/com/NcuWeather/android/logic/network/WeatherService.kt
@@ -0,0 +1,9 @@
+package com.NcuWeather.android.logic.network
+
+import retrofit2.http.GET
+
+interface WeatherService {
+
+   // @GET("v2.5/${SunnyWeatherApplication.TOKEN}")
+
+}
