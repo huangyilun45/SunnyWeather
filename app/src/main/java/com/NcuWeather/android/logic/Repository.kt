0f58b1b0fbfbdc97f3d462162def15795d2f5f0b
1006-Project <2394716733@qq.com>
2021-01-04 13:41:50 +0800

    second commit

---
+++ app/src/main/java/com/NcuWeather/android/logic/Repository.kt
@@ -0,0 +1,26 @@
+package com.NcuWeather.android.logic
+
+import androidx.lifecycle.liveData
+import com.NcuWeather.android.logic.model.Place
+import com.NcuWeather.android.logic.network.NcuWeatherNetwork
+import kotlinx.coroutines.Dispatchers
+import java.lang.Exception
+
+
+object Repository {
+
+    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            val placeResponse = NcuWeatherNetwork.searchPlaces(query)
+            if (placeResponse.status == "ok") {
+                val places = placeResponse.places
+                Result.success(places)
+            } else {
+                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
+            }
+        }catch (e:Exception){
+            Result.failure<List<Place>>(e)
+        }
+        emit(result)
+    }
+}
